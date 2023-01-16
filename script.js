65330ef4b611a7d33d8a6d4d16ae56b396dd083a
Surjith  K <surjithbhy007@gmail.com>
2023-01-16 05:41:14 +0530

    Updated script.js
    api calling update

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
-$(document).ready(function(){
 
-    var typed = new Typed('.typed', {
-        strings: ['Hello,', 'You can Find Weather info'],
-        smartBackspace: true,
-        loop:true,
-        typeSpeed:30,
-        backSpeed:30
-      });
-      
+ 
+function search(){
+    place=city.value
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=1c46b465a507578fb9601d4ad7f3673e&units=metric`).then(d=>d.json()).then(data=>displayreport(data))
+}
+
+function displayreport(details){
+
+    data=`<div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="col-md-8 col-lg-6 col-xl-4">
+
+      <div class="card" style="color: #4B515D; border-radius: 35px;">
+        <div class="card-body p-4">
+
+          <div class="d-flex">
+            <h6 class="flex-grow-1">${(place).toUpperCase()}</h6>
+            <h6><span id="time"></span></h6>
+          </div>
+
+          <div class="d-flex flex-column text-center mt-5 mb-4">
+            <h6 class="display-4 mb-0 font-weight-bold" style="color: #1C2331;"> ${details.main.temp}°C </h6>
+            <span class="small" style="color: #868B94">${details.weather[0].main}</span>
+          </div>
+
+          <div class="d-flex align-items-center">
+            <div class="flex-grow-1" style="font-size: 1rem;">
+              <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span class="ms-1"> ${details.wind.speed} km/h</span></div>
+              <div><i class="fas fa-tint fa-fw" style="color: #868B94;"></i> <span class="ms-1"> ${details.main.humidity}% </span>
+              </div>
+              <div><i class="fas fa-sun fa-fw" style="color: #868B94;"></i> <span class="ms-1"> ${details.sys.sunrise}</span>
+              </div>
+            </div>
+            <div>
+              <img src="http://openweathermap.org/img/wn/${details.weather[0].icon}@2x.png"
+                width="100px">
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </div>`
+
+  result.innerHTML=data
+
+    }
+
+    document.getElementById("ti").onmouseover = function() {mouseOver()};    
+    function mouseOver() {
+      document.getElementById("ti").style.fontSize = "50px";
+    }
+    document.getElementById("ti").onmouseout = function() {mouseOut()};    
+    function mouseOut() {
+      document.getElementById("ti").style.fontSize = "";
+    }
 
 
 
-    $("#place").on("keyup",function(plac){
-        var cityname = plac.target.value;
-        const APIKey = "1c46b465a507578fb9601d4ad7f3673e";
-
-        //server request
-        $.ajax({
-
-            url : `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`
-        }).done(function(wethrdata){
-            console.log(wethrdata);
-            
-
-            $("#profile").html(`
-            <div class"container">
-                <div class"row">
-
-                    <div class="card" style="width: 18rem;">
-                        <img src="http://openweathermap.org/img/wn/${wethrdata.weather[0].icon}@2x.png" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Weather: ${wethrdata.weather[0].description}</h5>
-                            <p class="card-text">The Temperature at ${cityname} is ${wethrdata.main.feels_like} &#8451; and it feels like it is ${wethrdata.main.feels_like} &#8451;</p>
-                            <a href="https://www.google.com/search?q=${cityname}" target="_blank" class="btn btn-primary">Learn more about ${cityname}</a>
-                        </div>
-                    </div>
-
-                
-                </div>
-
-            </div>`
-            );
-
-        })
-    });
 
 
-
-})
